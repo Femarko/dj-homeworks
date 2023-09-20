e092e5f897acd5eafe5850df860214e9cd33d19a
Марк <fenster.mark@yandex.ru>
2023-09-20 12:44:40 +0300

    stats

--- request-handling/landing/app/views.py
+++ request-handling/landing/app/views.py
@@ -13,7 +13,12 @@ counter_click = Counter()
 
 def index(request):
     # Реализуйте логику подсчета количества переходов с лендига по GET параметру from-landing
-
+    if request.GET.get('from-landing', ''):
+        if request.GET['from-landing'] == 'test':
+            counter_click.update('t')
+        else:
+            counter_click.update('o')
+    # print(f'counter_click: {counter_click}')
     return render(request, 'index.html')
 
 
@@ -29,14 +34,26 @@ def landing(request):
     else:
         sutable_template = 'landing.html'
         counter_show.update('o')
-    print(counter_show)
+    # print(f'counter_show: {counter_show}')
     return render(request, sutable_template)
 
 
 def stats(request):
     # Реализуйте логику подсчета отношения количества переходов к количеству показов страницы
     # Для вывода результат передайте в следующем формате:
-    return render(request, 'stats.html', context={
-        'test_conversion': 0.5,
-        'original_conversion': 0.4,
-    })
+    if counter_click['o'] != 0:
+        original_conversion = counter_click['o'] / counter_show['o']
+    else:
+        original_conversion = 'данные отсутствуют, т.к. не было переходов'
+    if counter_click['t'] != 0:
+        test_conversion = counter_click['t'] / counter_show['t']
+    else:
+        test_conversion = 'данные отсутствуют, т.к. не было переходов'
+    print(f'original_conversion: {original_conversion}')
+    print(f'test_conversion: {test_conversion}')
+    context = {
+            'test_conversion': test_conversion,
+            'original_conversion': original_conversion
+        }
+
+    return render(request, 'stats.html', context)
