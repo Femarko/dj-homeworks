c191e6da72fe3f054c5849398658077d1f80ceea
Марк <fenster.mark@yandex.ru>
2023-09-25 15:30:14 +0300

    started pagination

--- request-handling/pagination/app/views.py
+++ request-handling/pagination/app/views.py
@@ -1,14 +1,17 @@
+from csv import DictReader
+from django.core.paginator import Paginator
 from django.shortcuts import render, redirect
 from django.urls import reverse
-
+from settings import BUS_STATION_CSV
 
 def index(request):
     return redirect(reverse(bus_stations))
 
-
 def bus_stations(request):
+    bus_stations = DictReader(BUS_STATION_CSV)
+    paginator = Paginator(bus_stations, 10)
     current_page = 1
-    next_page_url = 'write your url'
+    next_page_url = int(request.GET.get('page', 1))  # 'write your url'
     return render(request, 'index.html', context={
         'bus_stations': [{'Name': 'название', 'Street': 'улица', 'District': 'район'},
                          {'Name': 'другое название', 'Street': 'другая улица', 'District': 'другой район'}],
@@ -16,4 +19,3 @@ def bus_stations(request):
         'prev_page_url': None,
         'next_page_url': next_page_url,
     })
-
